b28652929b0345e6812aef0ca1a9f4af02e7feca
yutian81 <yutian88881@hotmail.com>
2025-09-11 18:46:20 +0800

    Update nat64.php

--- proxyip-check/nat64.php
+++ proxyip-check/nat64.php
@@ -378,8 +378,8 @@ function HTML($hostname, $网站图标, $BEIAN, $临时TOKEN) {
         <button class="check-btn" onclick="checkNAT64()">🚀 开始检测</button>
         <div class="loading" id="loading"><div class="loading-spinner"></div></div>
         <div class="result" id="result"></div>
+        <div class="footer">$BEIAN</div>
     </div>
-    <div class="footer">$BEIAN</div>
     <div class="toast" id="toast"></div>
     <script>
         const hostname = '{$hostname_js}';
