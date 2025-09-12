735de6a60402a3da5c9340bd38bc217e4cfdc7f8
yutian81 <yutian88881@hotmail.com>
2025-09-12 10:57:11 +0800

    Create config.php

---
+++ proxyip-check/config.php
@@ -0,0 +1,18 @@
+<?php
+// =======================================================
+// == 统一配置文件 (config.php)
+// =======================================================
+
+define('ICO', 'https://cf-assets.www.cloudflare.com/dzlvafdwdttg/19kSkLSfWtDcspvQI5pit4/c5630cf25d589a0de91978ca29486259/performance-acceleration-bolt.svg');
+
+define('TOKEN', '');
+
+define('URL302', '');
+
+define('URL', '');
+
+define('BEIAN', "<p>© 2025 ProxyIP 检测合集 | 基于 PHP 构建 | <a href='https://github.com/cmliu'>原作者 CMliu</a> | <a href='https://github.com/yutian81'>修改 Yutian81</a> | <a href='https://blog.811520.xyz'>QingYun Blog</a></p>");
+
+define('IMG', '');
+
+?>
